9df1c1994e6f51f9ead9d4d739adb37f77f070d6
pls19961219@gmail.com <pls19961219@gmail.com>
2019-11-15 19:03:53 -0500

    boundingBox updated

--- boundingBox.py
+++ boundingBox.py
@@ -1,10 +1,30 @@
 import cv2
 import createMask
+import numpy as np
+import matplotlib.pyplot as plt
+import imutils
+from PIL import Image
+def boundingBox(firstFrame, maskNum):
+    coor_matrix = np.zeros((maskNum,4))
 
+    for ii in range(maskNum):
+        mask = createMask.main(firstFrame)
+        i,j = np.where(mask==1)
+        # compute the x_min,x_max,y_min,y_max
+        y_min = np.min(i)
+        y_max = np.max(i)
 
-def boundingBox(firstFrame):
-    mask = createMask.main(firstFrame)
-    x,y,w,h = cv2.boundingRect(mask) # (x,y) is the top-left coordinate of the rectangle and (w,h) be its width and height.
-    cv2.rectangle(mask,(x,y),(x+w,y+h),(0,255,0),2)
+        x_min = np.min(j)
+        x_max = np.max(j)
+        # construct matrix
+        w = x_max - x_min
+        h = y_max - y_min
+        coor_matrix[ii,0] = x_min
+        coor_matrix[ii,1] = y_min
+        coor_matrix[ii,2] = w
+        coor_matrix[ii,3] = h
 
-    return None
+    return coor_matrix
+if __name__ == '__main__':
+    coor_matrix = boundingBox('firstFrame.jpg', 2)
+    print(coor_matrix)
